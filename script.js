46d9c5f5ed373e564fdd9d0cd4f90a6db4c8397a
Deividas <puzeras@gmail.com>
2018-09-16 17:25:46 +0100

    fixing all second task

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const request = require('request');
 const zlib = require('zlib');
 const JSONStream = require('JSONStream')
 
+
 ////TASK 1
 
 
@@ -12,10 +13,13 @@ function getStreetNames() {
         'Accept-Encoding': 'gzip'
     };
 
-    request({url:'https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json', 'headers': headers})
+    request({
+        url: 'https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json',
+        'headers': headers
+    })
         .pipe(zlib.createGunzip())
         .pipe(JSONStream.parse('features.*'))
-        .on('data',function (chunk) {
+        .on('data', function (chunk) {
             console.log(chunk.properties.STREET)
         })
 
@@ -27,75 +31,61 @@ function getStreetNames() {
 
 const taskTwo = (function () {
 
-    let urls = [];
-    let highPriorityReq = false;
-    let values = {
-        reqPerTime: 0,
-        numberOfRequests: 0,
-        intervalTime: 0
+    let requests = [];
+    let timer = 0;
+
+
+    let addRequest = function (value) {
+        requests.push(value);
     };
 
-    function queue(values, cb) {
+    let addHighPriorityRequest = function (value) {
+        requests.unshift(value)
+    };
 
-        let count = 1;
-        let interval = setInterval(
-            function () {
-                if (count >= values.numberOfRequests) {
-                    clearInterval(interval);
+
+    let renRequests = function (atTheTime ,delay ) {
+        let count = 0;
+            for (let i = 0; i < requests.length; i++) {
+                for (let j = 0; j < atTheTime; j++) {
+                    timer = setTimeout( function(){
+                        if(count < requests.length){
+                          fetch(requests[count])
+                                .then(response => response.json())
+                                .then(data => {
+                                    console.log(data)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                });
+                        }else{
+                            clearTimeout(timer)
+                        }
+                        count++;
+                    }, i*delay);
                 }
-                for (let i = 0; i < values.reqPerTime; i++) {
-                    if (urls.length > 1 && i >= 1) {
-                        highPriorityReq = true;
-                    }
-                    cb()
-                }
-                highPriorityReq = false;
-                count++;
-                console.log(count)
-
-            }, values.intervalTime);
-    }
-
-
-    let runRequestQueue = function () {
-
-        urls.length && urls.length < 3 ?
-            queue(values, function () {
-                fetch(urls[+highPriorityReq])
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data)
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-            })
-        : console.log('No URL or more then 2 URL\'s added')
+            }
     };
 
-
-    let addRequestURL = function (value) {
-        urls.unshift(value)
-    };
-
-    let setValues = function (reqPerTime, numberOfRequests, intervalTime) {
-        values = {reqPerTime, numberOfRequests, intervalTime};
-    };
-
-
-    return {
-        runRequestQueue: runRequestQueue,
-        addRequestURL: addRequestURL,
-        setValues: setValues
+    return{
+        addRequest: addRequest,
+        renRequests: renRequests,
+        addHighPriorityRequest: addHighPriorityRequest
     }
 
 })();
 
+
 getStreetNames();
-taskTwo.addRequestURL('https://jsonplaceholder.typicode.com/todos/1');
-taskTwo.addRequestURL('https://jsonplaceholder.typicode.com/todos/2');
-taskTwo.setValues(5,20, 1000);
-taskTwo.runRequestQueue();
+
+
+for (let i = 1; i < 100; i++) {
+    taskTwo.addRequest(`https://jsonplaceholder.typicode.com/todos/${i}`)
+}
+taskTwo.addHighPriorityRequest(`https://jsonplaceholder.typicode.com/todos/101`);
+taskTwo.renRequests(5,1000);
+
+
 
 
 
